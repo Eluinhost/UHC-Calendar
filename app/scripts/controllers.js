38db11eb831d344771fcad8584b7f86ed0e4e9f0
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-07 15:22:22 +0100

    add pull to refresh for the posts page

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -18,9 +18,18 @@ angular.module('UHCCalendar.controllers', [])
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 })
 
-.controller('LatestCtrl', ['$scope', 'RedditPostsService', function($scope, SearchPostsService) {
-    SearchPostsService.query().then(function(valid, invalid) {
-        $scope.posts = valid;
-        $scope.unparsed = invalid;
-    });
-}]);
+    .controller('LatestCtrl', ['$scope', 'RedditPostsService', function($scope, SearchPostsService) {
+        $scope.posts = [];
+        $scope.unparsed = [];
+
+        $scope.updatePosts = function() {
+            SearchPostsService.query().then(function (valid, invalid) {
+                $scope.posts = valid;
+                $scope.unparsed = invalid;
+            }).finally(function() {
+                $scope.$broadcast('scroll.refreshComplete');
+            });
+        };
+
+        $scope.updatePosts();
+    }]);
